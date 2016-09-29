f3978e77c81a45c3de5fb449056f040f38e174cb
DavidtheDJ <DavidtheDJ00@gmail.com>
2016-09-28 21:32:20 -0700

    Exercise 2.

---
+++ BinarySearch.py
@@ -0,0 +1,21 @@
+#David Justice
+#9-22-16
+#Binary Search
+
+
+def search(numlist, itemSearch):
+    found = False
+
+
+
+def start():
+    numberList = [1,4,6,8,12,15,18,19,24,27,31,42,43,58]
+    item = int(input("What number are you looking for?"))
+    isItFound = search(item,numberlist)
+
+    search()
+    
+    if isItFound:
+        print("Your number is there!")
+    else:
+        print("Your number is not there.")
